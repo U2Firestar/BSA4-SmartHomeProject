9fcff5ca575d944e9ef51a3a68daa1c56c3dc87e
RaphaelAmann <85880618+RaphaelAmann@users.noreply.github.com>
2021-06-14 14:56:53 +0200

    deteted directory ui

--- ui/header.inc.php
+++
@@ -1,7 +0,0 @@
-<table align='center' border='0' bgcolor='#fcba03' width='90%'>
-  <tr>
-    <td align='center' width='10%'><a href='index.php'><img src='sym/house.svg' width='30px' alt='Zur Startseite'></a></td>
-    <td align='center' class='seitentitel'>SmartHome</td>
-    <td align='center' width='10%'></td>
-  </tr>
-</table>
