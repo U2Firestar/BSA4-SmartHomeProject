2a31b764b3d03054b09951b52573e6ee918b372f
mia <maschildbeck@gmail.com>
2021-06-20 19:12:56 +0200

    changed route paths

--- REST_api/app.py
+++ REST_api/app.py
@@ -7,61 +7,126 @@ app = Flask(__name__)
 
 # a Python object (dict):
 # TODO make persistent in json file
-resources = {
+lights = {
 	"light1": 0,
 	"light2": 30, 
-	"light3": 100,
+	"light3": 100
+}
+
+shutters = {
 	"shutter1": 60,
 	"shutter2": 80
 }
 
-@app.route('/resources',methods=['GET'])
-def resources_get():
+# lights: ----------------------------------------------
+# all lights: 
+@app.route('/resource/lights',methods=['GET'])
+def lights_get():
 	# convert into JSON:
-	x = json.dumps(resources)
+	x = json.dumps(lights)
 	# the result is a JSON string:
 	return x
 
-@app.route('/resource/<item>',methods=['GET'])
-def resourceX_get(item):
-	if item in resources:
-		return str(resources[item])
+# one specific light:
+@app.route('/resource/lights/<item>',methods=['GET'])
+def lightX_get(item):
+	if item in lights:
+		return str(lights[item])
 	else:
 		return f"Can't find {escape(item)}."
 
-@app.route('/resource/<item>',methods=['PUT'])
-def resourceX_put(item):
-	global resources
-	if item in resources:
-		# update resource:
+@app.route('/resource/lights/<item>',methods=['PUT'])
+def lightX_put(item):
+	global lights
+	if item in lights:
+		# update light:
 		state = request.args.get("state")
-		resources[item] = state
-		return str(resources[item])
+		try:
+			lights[item] = int(state)
+		except ValueError:
+			lights[item] = 0	
+		return str(lights[item])
 	else:
 		# resource not found:
 		return f"{escape(item)} not found."
 
-@app.route('/resource/<item>',methods=['POST'])
-def resourceX_post(item):
-	global resources
-	if item in resources:
+@app.route('/resource/lights/<item>',methods=['POST'])
+def lightX_post(item):
+	global lights
+	if item in lights:
 		# set value to 0:
-		resources[item] = 0
+		lights[item] = 0
 		return str(0)
 	else:
 		# add new resource:
-		resources[item] = 0
+		lights[item] = 0
 		return str(0)
 
-@app.route('/resource/<item>',methods=['DELETE'])
-def resourceX_delete(item):
-	global resources
-	if item in resources:
+@app.route('/resource/lights/<item>',methods=['DELETE'])
+def lightX_delete(item):
+	global lights
+	if item in lights:
 		# remove entry with key <item>:
-		del resources[item]; 
+		del lights[item]; 
 		return f"Removed {escape(item)}."
 	else:
 		return f"{escape(item)} not found."
+
+
+# shutters: -------------------------------------------------
+# all shutters: 
+@app.route('/resource/shutters',methods=['GET'])
+def shutters_get():
+	# convert into JSON:
+	x = json.dumps(shutters)
+	# the result is a JSON string:
+	return x
+
+# one shutter: 
+@app.route('/resource/shutters/<item>',methods=['GET'])
+def shutterX_get(item):
+	if item in shutters:
+		return str(shutters[item])
+	else:
+		return f"Can't find {escape(item)}."
+
+@app.route('/resource/shutters/<item>',methods=['PUT'])
+def shutterX_put(item):
+	global shutters
+	if item in shutters:
+		# update resource:
+		state = request.args.get("state")
+		try:
+			shutters[item] = int(state)
+		except ValueError:
+			shutters[item] = 0
+		return str(shutters[item])
+	else:
+		# resource not found:
+		return f"{escape(item)} not found."
+
+@app.route('/resource/shutters/<item>',methods=['POST'])
+def shutterX_post(item):
+	global shutters
+	if item in shutters:
+		# set value to 0:
+		shutters[item] = 0
+		return str(0)
+	else:
+		# add new resource:
+		shutters[item] = 0
+		return str(0)
+
+@app.route('/resource/shutters/<item>',methods=['DELETE'])
+def shutterX_delete(item):
+	global shutters
+	if item in shutters:
+		# remove entry with key <item>:
+		del shutters[item]; 
+		return f"Removed {escape(item)}."
+	else:
+		return f"{escape(item)} not found."
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
